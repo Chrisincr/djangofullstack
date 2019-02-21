83d02c93761e05e115e2478d5145c4bab250f344
Chris Thompson <chrisincr@gmail.com>
2019-02-20 20:50:17 -0600

    updated edit/update forms for simple validation

--- semiresttvshows/apps/tvShows/models.py
+++ semiresttvshows/apps/tvShows/models.py
@@ -1,4 +1,32 @@
 from django.db import models
+import datetime
+
+
+class ShowManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        # add keys and values to errors dictionary for each invalid field
+        if len(postData['title']) < 5:
+            errors["title"] = "Show Title should be at least 5 characters"
+        if len(postData['network']) < 2:
+            errors["network"] = "Show network should be at least 2 characters"
+        try:
+            datetime.datetime.strptime(postData['releaseDate'],'%Y-%m-%d')
+            
+        except ValueError:
+            errors["releaseDate"]= "Show Release Date must be in format of 'YYYY-MM-DD'"
+        
+        try:
+            if datetime.datetime.strptime(postData['releaseDate'],'%Y-%m-%d') > datetime.datetime.today():
+                errors["releaseDate"]="Show Release Date must be before Today"
+        except ValueError:
+            errors["releaseDate"]= "Show Release Date must be in format of 'YYYY-MM-DD'"
+
+        if len(postData['description']) < 10:
+            errors["description"] = "Show description should be at least 10 characters"
+        return errors
+
+
 
 class Shows(models.Model):
     title = models.CharField(max_length = 255)
@@ -7,3 +35,4 @@ class Shows(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowManager()
--- semiresttvshows/apps/tvShows/views.py
+++ semiresttvshows/apps/tvShows/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,HttpResponse,redirect
+from django.contrib import messages
 from apps.tvShows.models import *
 
 def shows(request):
@@ -12,13 +13,21 @@ def shows_new(request):
     return render(request, "tvShows/newshow.html")
 
 def shows_create(request):
+    errors = Shows.objects.basic_validator(request.POST)
 
-    context={
-        'show': request.POST,
-    }
-    print(context['show']['date'])
-    showid = Shows.objects.create(title=context['show']['title'], network=context['show']['network'],releaseDate=str(context['show']['date']),description=str(context['show']['description']))
-    return redirect("/shows/"+str(showid.id))
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/shows/new')
+    else:
+
+        context={
+            'show': request.POST,
+        }
+        
+        showid = Shows.objects.create(title=context['show']['title'], network=context['show']['network'],releaseDate=str(context['show']['releaseDate']),description=str(context['show']['description']))
+        messages.success(request, "Show successfully added")
+        return redirect("/shows/"+str(showid.id))
 
 def shows_show(request,id):
     context = {
@@ -34,20 +43,27 @@ def shows_edit(request,id):
     return render(request,"tvShows/editshow.html",context)
 
 def shows_update(request,id):
-    context = {
-        'show': Shows.objects.get(id=id),
-        'updates':request.POST,
-    }
+    errors = Shows.objects.basic_validator(request.POST)
+
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/shows/'+str(id)+'/edit')
+    else:
+        context = {
+            'show': Shows.objects.get(id=id),
+            'updates':request.POST,
+        }
     
-    print(context['updates'])
-    print(context['updates']['releaseDate'])
-    context['show'].title =context['updates']['title']
-    context['show'].network =context['updates']['network']
-    context['show'].releaseDate =context['updates']['releaseDate']
-    context['show'].description =context['updates']['description']
-    context['show'].save()
-    
-    return redirect("/shows/"+str(id))
+        print(context['updates'])
+        print(context['updates']['releaseDate'])
+        context['show'].title =context['updates']['title']
+        context['show'].network =context['updates']['network']
+        context['show'].releaseDate =context['updates']['releaseDate']
+        context['show'].description =context['updates']['description']
+        context['show'].save()
+        
+        return redirect("/shows/"+str(id))
 
 def shows_destroy(request,id):
     context = {
